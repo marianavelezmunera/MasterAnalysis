b331caba52920828c8965b49b040953dd03bc343
Mariana Vélez Múnera <marianavelez.35@gmail.com>
2024-10-06 19:32:19 -0600

    6 oct 2024

--- 4.2-SampleSubsetBacteria.R
+++
@@ -1,8 +0,0 @@
-# Subset per sample
-bacterias_rare@sam_data[bacterias_rare@sam_data=="Filosfera"]<-"Phyllosphere"
-bacterias_rare@sam_data[bacterias_rare@sam_data=="Rizosfera"]<-"Rhizosphere"
-bacterias_rare@sam_data[bacterias_rare@sam_data=="Suelo"]<-"Soil"
-
-bacterias_filosfera<-subset_samples(bacterias_rare,Tipo_muestra=="Phyllosphere")
-bacterias_suelo<-subset_samples(bacterias_rare,Tipo_muestra=="Soil")
-bacterias_rizosfera<-subset_samples(bacterias_rare,Tipo_muestra=="Rhizosphere")
