25f550d5e04131cfd0c4937c4d6a0696a350b5fc
Mariana Vélez Múnera <marianavelez.35@gmail.com>
2024-10-16 12:22:30 -0600

    16 oct 2024

--- 11.1-RDAFungi.R
+++ 11.1-RDAFungi.R
@@ -37,8 +37,8 @@ rda_hongos_filo_plot<-rda_hongos_filo %>%
     constraints = colnames(rda_hongos_filo@sam_data)[c(18,20,22,23,24,28,35,37)],method = "RDA",
     scale_cc = FALSE) %>% 
   ord_plot(colour="Altitud",plot_taxa = clases_rda_hongos[c(-6,-4,-5,-9),1],size=3,
-           constraint_lab_style = constraint_lab_style(size = 4),constraint_vec_length = 4,tax_vec_length = 4,auto_caption = NA,
-           tax_lab_style = tax_lab_style(size = 3))+
+           constraint_lab_style = constraint_lab_style(size = 5),constraint_vec_length = 4,tax_vec_length = 4,auto_caption = NA,
+           tax_lab_style = tax_lab_style(size = 4.5))+
   scale_color_manual(name="Elevation",values=colores)+
   theme_biome_utils()+
   ggtitle("c. Fungal phyllosphere")+
@@ -48,7 +48,7 @@ rda_hongos_filo_plot<-rda_hongos_filo %>%
   theme(legend.text = element_text(size=12))
 rda_hongos_filo_plot
 
-# Phyllosphere
+# Rhizosphere
 
 rda_hongos_rizo <- tax_fix(hongos_rizosfera)
 rda_hongos_rizo <- phyloseq_validate(rda_hongos_rizo, remove_undetected = TRUE)
@@ -87,8 +87,8 @@ rda_hongos_rizo_plot<-rda_hongos_rizo %>%
     constraints = colnames(rda_hongos_rizo@sam_data)[c(18,20,22,23,24,28,35,37)],method = "RDA",
     scale_cc = FALSE) %>% 
   ord_plot(colour="Altitud",plot_taxa = clases_rda_hongos[c(-4,-9,-1,-10,-3),1],size=3,
-           constraint_lab_style = constraint_lab_style(size = 4),constraint_vec_length = 4,tax_vec_length = 4,auto_caption = NA,
-           tax_lab_style = tax_lab_style(size = 3))+
+           constraint_lab_style = constraint_lab_style(size = 5),constraint_vec_length = 4,tax_vec_length = 4,auto_caption = NA,
+           tax_lab_style = tax_lab_style(size = 4.5))+
   scale_color_manual(name="Elevation",values=colores)+
   theme_biome_utils()+
   ggtitle("d. Fungal rhizosphere")+
